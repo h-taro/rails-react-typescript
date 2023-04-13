bdf55acf62caa87c08daba02aa87922873f3f0ed
平石太郎 <taro.hiraishi@play.jp>
2023-04-14 07:11:45 +0900

    Initial Commit

---
+++ app/javascript/packs/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app')
+  const root = createRoot(app)
+  root.render(<App />)
+})
